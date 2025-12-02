580dba05ce47fa854b5b38ae186bad86ad279d6d
Rayane-Aboud <jr_aboud@esi.dz>
2025-12-02 15:15:13 +0100

    added abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 // src/main.rs
 use anyhow::Result;
 
-fn main() -> Result<()> {
-    
-
-    Ok(())
+pub struct Scheduler {
+  pub workers: Vec<Worker>,
+  pub tasks: Vec<Task>
 }
+
+//
+pub trait Resource{}
+//
+pub struct Cpu;
+impl Resource for Cpu {}
+//
+pub struct Ram;
+impl Resource for Ram{}
+
+pub struct Worker {
+  pub task: Task,
+
+}
+pub struct Task;
+impl Future for Task {
+    type Output;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+        todo!()
+    }
+}
+
+fn main() -> Result<()> {
+  
+  Ok(())
+}
+
+/* 
+scheduler that runs wasm files while knowing their resources
+
+*/
