bf48eabe76b476307efd48a7be10ac561a71f1e5
Rayane-Aboud <jr_aboud@esi.dz>
2025-12-03 09:19:03 +0100

    defined a supervisor

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
+use std::{collections::HashMap, sync::Arc};
+
 // src/main.rs
 use anyhow::Result;
 
-pub struct Scheduler {
-  pub workers: Vec<Worker>,
-  pub tasks: Vec<Task>
+pub struct Supervisor {
+  pub nodes: HashMap<
+    Arc<str>, //node id
+    Node  //node
+  >
 }
 
-//
-pub trait Resource{}
-//
-pub struct Cpu;
-impl Resource for Cpu {}
-//
-pub struct Ram;
-impl Resource for Ram{}
-
-pub struct Worker {
-  pub task: Task,
-
-}
-pub struct Task;
-impl Future for Task {
-    type Output;
-
-    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
-        todo!()
+impl Supervisor {
+  pub fn new(){
+    Supervisor {
+      nodes: HashMap()
     }
+  }
+  pub fn monitor_health(){
+
+  }
+}
+
+pub struct Node {
+  pub id: Arc<str>,
+  pub url: Arc<str>,
+  pub status: Status,
+}
+pub enum Status {
+  Alive,
+  Overloaded,
+  Dead, 
 }
 
 fn main() -> Result<()> {
